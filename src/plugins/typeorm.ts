9afb600aef50e7c5b21bb847a41f60093efc22c7
armada45-pixel <armada1@hotmail.co.th>
2022-06-05 19:30:04 +0700

    remove gr-shutdown

--- src/plugins/typeorm.ts
+++ src/plugins/typeorm.ts
@@ -7,24 +7,6 @@ export default fp<FastifyTypeormOpts>(async (app) => {
     config: { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME },
   } = app
 
-  // const connectionOptions: fastifyTypeorm.FastifyTypeormOptions = {
-  //   type: "postgres",
-  //   host: DB_HOST,
-  //   port: DB_PORT,
-  //   username: DB_USERNAME,
-  //   password: DB_PASSWORD,
-  //   database: DB_NAME,
-  //   synchronize: true,
-  //   logging: false,
-  //   entities: ["src/entities/**/*.ts"],
-  //   // migrations: ["src/migrations/**/*.ts"],
-  //   // subscribers: ["src/subscribers/**/*.ts"],
-  //   cli: {
-  //     entitiesDir: "src/entities",
-  //     // migrationsDir: "src/migrations",
-  //     // subscribersDir: "src/subscribers",
-  //   },
-  // }
   app.register(fastifyTypeorm, {
     type: "postgres",
     host: DB_HOST,
